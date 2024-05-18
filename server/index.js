6a53e116e8c9f7d3a8b7195cafb53eb1ab111e7e
mattvie <mvmf@cin.ufpe.br>
2024-05-18 18:07:11 -0300

    URL update fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://methods.mateus.vercel.app"],
+        origin: ["https://mern-methods.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
