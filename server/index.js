28229536d37de8a804e10883d62fb9472ecd2ccf
Mateus Viégas <mvmf@cin.ufpe.br>
2024-05-18 19:45:26 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://mern-methods-server.vercel.app"],
+        origin: ["https://mern-methods.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
